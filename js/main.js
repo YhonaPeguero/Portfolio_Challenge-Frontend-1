3a3635856c73552f07bf4e773b8bc5fb77ef56b1
Yhonatan Peguero <2017-0516@unad.edu.do>
2022-05-19 22:57:21 -0400

    add new project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(function () {
-  $(".toggle").on("click", function () {
-    if ($(".menu__item").hasClass("active")) {
-      $(".menu__item").removeClass("active");
-      $(this).find("a").html("<i class='fas fa-bars'></i>");
-    } else {
-      $(".menu__item").addClass("active");
-      $(this).find("a").html("<i class='fas fa-times'></i>");
-    }
-  });
-});
+// $(function () {
+//   $(".toggle").on("click", function () {
+//     if ($(".menu__item").hasClass("active")) {
+//       $(".menu__item").removeClass("active");
+//       $(this).find("a").html("<i class='fas fa-bars'></i>");
+//     } else {
+//       $(".menu__item").addClass("active");
+//       $(this).find("a").html("<i class='fas fa-times'></i>");
+//     }
+//   });
+// });
